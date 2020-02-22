21abea6c6a369610e643fa9a48657552718c0228
nam24 <namratatripathi24@gmail.com>
2020-02-23 00:42:09 +0530

    set-up

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-   # This is a placeholder file
+# This is a placeholder file
 
 from flask import Flask, send_from_directory
 import random
+import json
+from flask import jsonify
+
+import urllib.request
 
 app = Flask(__name__)
-
+student_json_url = 'https://raw.githubusercontent.com/Korku02/iitd-userdata/master/js/student.json'
 # Path for our main Svelte page
 @app.route("/")
 def base():
-    return send_from_directory('client/public', 'index.html')
+    return {'version': 1, 'author': 'Namrata Tripathi'}
 
 # Path for all the static files (compiled JS/CSS, etc.)
 @app.route("/<path:path>")
@@ -19,6 +23,13 @@ def home(path):
 @app.route("/rand")
 def hello():
     return str(random.randint(0, 100))
+
+
+@app.route("/students")
+def student_data():
+    with urllib.request.urlopen('https://raw.githubusercontent.com/Korku02/iitd-userdata/master/js/student.json') as url:
+        data = json.loads(url.read().decode())
+        return jsonify(data)
 
 
 if __name__ == "__main__":
